0f92a5e2e6b682f87111332845d258a7f67bf66b
codeWonderland <alice.easter@mymail.champlain.edu>
2018-04-17 22:18:04 -0400

    Preliminary connetions to websocket in place

--- app/src/main/java/edu/champlain/hangman_client/MainActivity.kt
+++ app/src/main/java/edu/champlain/hangman_client/MainActivity.kt
@@ -2,11 +2,29 @@ package edu.champlain.hangman_client
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocketListener
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    val serverUrl = "ws://172.19.35.66:3000/ws"
+    val name = "codeWonderland"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = OkHttpClient.Builder()
+                .readTimeout(3, TimeUnit.SECONDS)
+                .build()
+
+        val request = Request.Builder()
+                .url(serverUrl)
+                .build()
+
+        val webSocket = client.newWebSocket(request, HangmanWebSocket(name))
     }
 }
+
